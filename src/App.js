8c3982d2f6f5d0c71a017d8a1c0bc00607cad93a
Fabio Hsu <Fabio90h@Fabios-MBP.attlocal.net>
2018-06-13 21:57:30 -0400

    Fix layout and added stats

--- src/App.js
+++ src/App.js
@@ -23,15 +23,23 @@ class App extends Component {
     return (
         <div className="App">
           <div id='header'><div id='messicrying'><div></div></div><span>World Cup 2018</span></div>
-          <div>
+          <div id='rulesStats'>
             <ul id='pointSystem'>
-              <h3>Points System</h3>
+              <h3>Point System</h3>
                 <li><strong>Round of 16:</strong> +2 (32 possible points)</li>
                 <li><strong>Quarterfinal:</strong> +3 (24 possible points)</li>
                 <li><strong>Semifinal:</strong> +8 (32 possible points)</li>
                 <li><strong>Final:</strong> +15 (30 possible points)</li>
                 <li><strong>Champion:</strong> +30 (30 possible points)</li>
                 <li><strong>3rd Place:</strong> +5 (5 possible points)</li>
+            </ul>
+            <ul id='stats'>
+              <h3>People's choice to win</h3>
+              <li>1. Brazil <strong>46%</strong></li>
+              <li>2. Germany <strong>25%</strong></li>
+              <li>3. Argentina <strong>21%</strong></li>
+              <li>4. France <strong>4%</strong></li>
+              <li>4. Spain <strong>4%</strong></li>
             </ul>
           </div>
           <div className='userDiv'>
